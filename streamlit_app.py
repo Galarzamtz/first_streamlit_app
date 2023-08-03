87f33c7a056bcd3d276b65b2efde21f0e63ce506
Galarzamtz <38111312+Galarzamtz@users.noreply.github.com>
2023-08-03 10:42:34 -0600

    Update streamlit_app.py
    Added choose menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ streamlit.dataframe(fruits_to_show)
 #API integration
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
 
 
 # write your own comment -what does the next line do?  - Normalizes the json data structure
