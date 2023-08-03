a6077c53131685b1bf1c9fa52c09bb24fcc36780
Galarzamtz <38111312+Galarzamtz@users.noreply.github.com>
2023-08-03 15:16:03 -0600

    Modified streamlit_app.py
    Added SQL Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+erimport streamlit
 import pandas
 import snowflake.connector
 
@@ -6,8 +6,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
