89a9a87280aff08b1ec8f7ae173eeb4117ef6923
Galarzamtz <38111312+Galarzamtz@users.noreply.github.com>
2023-08-03 15:43:39 -0600

    Update streamlit_app.py
    new block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,14 +26,22 @@ streamlit.dataframe(fruits_to_show)
 
 #API integration
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered: ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error()
 
 # write your own comment -what does the next line do?  - Normalizes the json data structure
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do? - Shows a table with the formatted json data
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 
